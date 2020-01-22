aca9e1679955ef0d5a1bf9af388a89526664c96d
Caio Santos <caiosantosfreitas@hotmail.com>
2020-01-22 07:02:45 -0300

    index implements

--- index.js
+++ index.js
@@ -12,7 +12,11 @@ const app = express();
 const port = process.env.PORT || "3000";
 
 app.set('view engine', 'ejs')
-app.set('views', path.join(__dirname, 'views'));
+app.use(express.static('public'))
+app.use('/styles/bulma.min.css', express.static('node_modules/bulma/css/bulma.min.css'))
+app.use('/styles', express.static('node_modules/@fortawesome/fontawesome-free/css'))
+app.use('/styles/fa.min.css', express.static('node_modules/@fortawesome/fontawesome-free/css/all.min.css'))
+app.use('/scripts/fa.min.js', express.static('node_modules/@fortawesome/fontawesome-free/js/all.min.js'))
 
 app.use(require('./api/routes/index'));
 
